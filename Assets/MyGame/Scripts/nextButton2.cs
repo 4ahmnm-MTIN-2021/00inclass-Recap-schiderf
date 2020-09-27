3d81734245b2be23869efca7b9e1e6d957d1edce
schiderf <felix.schider02@htl-salzburg.ac.at>
2020-09-27 19:40:32 +0200

    init commit

---
+++ Assets/MyGame/Scripts/nextButton2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class nextButton2 : MonoBehaviour
+{
+    public Button btn;
+    public Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            
+        }
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
