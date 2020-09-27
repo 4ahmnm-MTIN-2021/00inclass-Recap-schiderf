18381c63d42a494b4b48100e5381b53308300767
schiderf <felix.schider02@htl-salzburg.ac.at>
2020-09-27 20:42:48 +0200

    tried coding the calculator

--- Assets/MyGame/Scripts/calcScript.cs
+++ Assets/MyGame/Scripts/calcScript.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class calcScript : MonoBehaviour
 {
+    public Button nxtbtn;
+    public InputField numb1;
+    public InputField numb2;
+    public Text solution;
+    public Button calc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+
+
+
+    private int addNumbers()
+    {
+        int tempResult = int.Parse(numb1.text) + int.Parse(numb2.text);
+        return tempResult;
+    }
+
+    public void SetResult()
+    {
+        solution.text = addNumbers().ToString();
     }
 }
